a8c9a2308c1bbf57995eefa618fc97f2825e8655
Rishabh Raj <rishabhraj240405@gmail.com>
2024-08-24 21:35:17 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 7;
+
+        ++a;
+        b--;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
